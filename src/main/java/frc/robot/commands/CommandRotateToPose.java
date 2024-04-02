f78daf860533edb099fdd535313d51e6d24c7ec0
Gaurav Salandri <salangau000@gmail.com>
2024-04-01 19:05:15 -0500

    attempted swerve rotation

--- src/main/java/frc/robot/commands/CommandRotateToPose.java
+++ src/main/java/frc/robot/commands/CommandRotateToPose.java
@@ -1,106 +1,105 @@
-// package frc.robot.commands;
+package frc.robot.commands;
 
-// import edu.wpi.first.math.controller.ProfiledPIDController;
-// import edu.wpi.first.math.geometry.Pose2d;
-// import edu.wpi.first.math.geometry.Rotation2d;
-// import edu.wpi.first.math.kinematics.ChassisSpeeds;
-// import edu.wpi.first.math.trajectory.TrapezoidProfile;
-// import edu.wpi.first.math.util.Units;
-// import edu.wpi.first.wpilibj.smartdashboard.SmartDashboard;
-// import edu.wpi.first.wpilibj2.command.Command;
-// import frc.robot.Constants;
-// import frc.robot.Constants.AllianceFlipUtil;
-// import frc.robot.Constants.ScoringConstants;
-// import frc.robot.subsystems.Swerve;
-// import frc.robot.subsystems.vision.ApriltagVision;
+import com.ctre.phoenix6.mechanisms.swerve.SwerveRequest;
 
-// public class CommandRotateToPose extends Command {
+import edu.wpi.first.math.controller.ProfiledPIDController;
+import edu.wpi.first.math.geometry.Pose2d;
+import edu.wpi.first.math.geometry.Rotation2d;
+import edu.wpi.first.math.kinematics.ChassisSpeeds;
+import edu.wpi.first.math.trajectory.TrapezoidProfile;
+import edu.wpi.first.math.util.Units;
+import edu.wpi.first.wpilibj.smartdashboard.SmartDashboard;
+import edu.wpi.first.wpilibj2.command.Command;
+import frc.robot.CommandSwerveDrivetrain;
+import frc.robot.Constants;
+import frc.robot.Constants.AllianceFlipUtil;
+import frc.robot.Constants.ScoringConstants;
+import frc.robot.subsystems.vision.ApriltagVision;
 
-//   private final Swerve swerve;
-//   private Rotation2d desiredAngle;
-//   private boolean fixedRotation;
-//   private ApriltagVision m_vision;
+public class CommandRotateToPose extends Command {
 
-//   private final ProfiledPIDController thetaController =
-//       new ProfiledPIDController(Constants.Swerve.angleKP, Constants.Swerve.angleKI, Constants.Swerve.angleKD, new TrapezoidProfile.Constraints(Constants.AutoConstants.kMaxAngularSpeedRadiansPerSecond, Constants.AutoConstants.kMaxAngularSpeedRadiansPerSecondSquared));
+  private final CommandSwerveDrivetrain swerve;
+  private Rotation2d desiredAngle;
+  private boolean fixedRotation;
+  private ApriltagVision m_vision;
+  SwerveRequest.FieldCentric drive;
 
-//   // public CommandRotateToPose(Swerve swerve, Rotation2d r) {
-//   //   this.swerve = swerve;
-//   //   this.desiredAngle = r;
-//   //   this.fixedRotation = true;
+  private final ProfiledPIDController thetaController =
+      new ProfiledPIDController(5, 0, 0, new TrapezoidProfile.Constraints(Constants.AutoConstants.kMaxAngularSpeedRadiansPerSecond, Constants.AutoConstants.kMaxAngularSpeedRadiansPerSecondSquared));
 
-//   //   thetaController.setTolerance(Units.degreesToRadians(1.7));
-//   //   thetaController.enableContinuousInput(-Math.PI, Math.PI);
+  public CommandRotateToPose(CommandSwerveDrivetrain swerve, Rotation2d r) {
+    this.swerve = swerve;
+    this.desiredAngle = r;
+    this.fixedRotation = true;
 
-//   //   addRequirements(swerve);
-//   // }
+    thetaController.setTolerance(Units.degreesToRadians(0.5));
+    thetaController.enableContinuousInput(-Math.PI, Math.PI);
 
-//   public CommandRotateToPose(Swerve swerve, ApriltagVision m_vision) {
-//     this.swerve = swerve;
-//     this.fixedRotation = false;
-//     this.m_vision = m_vision;
-//     thetaController.setTolerance(Units.degreesToRadians(1.7));
-//     thetaController.enableContinuousInput(-Math.PI, Math.PI);
+    addRequirements(swerve);
+  }
 
-//     addRequirements(swerve);
-//   }
+  public CommandRotateToPose(CommandSwerveDrivetrain swerve, ApriltagVision m_vision) {
+    this.swerve = swerve;
+    this.fixedRotation = false;
+    this.m_vision = m_vision;
+    thetaController.setTolerance(Units.degreesToRadians(0.5));
+    thetaController.enableContinuousInput(-Math.PI, Math.PI);
 
-//   @Override
-//   public void initialize() {
-//     Constants.Vision.autoAlignActice = true;
-//     // if (!fixedRotation){
-//     //   switch (ScoringConstants.currentScoringMode) {
-//     //     case PODIUM:
-//     //       this.desiredAngle= Constants.SATConstants.PODIUM.pose.getRotation();
-//     //       break;
-//     //     case SUBWOOFER:
-//     //       this.desiredAngle= Constants.SATConstants.SUB.pose.getRotation();
-//     //       break;
-//     //     case AMP:
-//     //       this.desiredAngle= Constants.SATConstants.AMP.pose.getRotation();
-//     //       break;
-//     //     default:
-//     //       break;
-//     //   }
-//     // }
+    addRequirements(swerve);
+  }
+
+  @Override
+  public void initialize() {
+    Constants.Vision.autoAlignActice = true;
+    if (!fixedRotation){
+      switch (ScoringConstants.currentScoringMode) {
+        case PODIUM:
+          this.desiredAngle= Constants.SATConstants.PODIUM.pose.getRotation();
+          break;
+        case AMP:
+          this.desiredAngle= Constants.SATConstants.AMP.pose.getRotation();
+          break;
+        default:
+          break;
+      }
+    }
 
       
-//     var currPose = swerve.getPose();
-//     this.desiredAngle = Rotation2d.fromDegrees(currPose.getRotation().getRadians() - Units.degreesToRadians(m_vision.getYaw()));
-//     SmartDashboard.putNumber("desiredAngle", desiredAngle.getDegrees());  
-//     thetaController.reset(currPose.getRotation().getRadians());
-//   }
+    var currPose = swerve.getState().Pose;
+    this.desiredAngle = Rotation2d.fromDegrees(currPose.getRotation().getRadians() - Units.degreesToRadians(m_vision.getYaw()));
+    SmartDashboard.putNumber("desiredAngle", desiredAngle.getDegrees());  
+    thetaController.reset(currPose.getRotation().getRadians());
+  }
 
-//   @Override
-//   public void execute() {
-//     var currPose = swerve.getPose();
-//     TeleopSwerve.goodStraightGyro = currPose.getRotation();
-//     double thetaVelocity =
-//         thetaController.calculate(
-//             currPose.getRotation().getRadians(), desiredAngle.getRadians());
+  @Override
+  public void execute() {
+    var currPose = swerve.getState().Pose;
+    double thetaVelocity =
+        thetaController.calculate(
+            currPose.getRotation().getRadians(), desiredAngle.getRadians());
 
-//     if (atGoal()) {
-//       thetaVelocity = 0.0;
-//     }
+    if (atGoal()) {
+      thetaVelocity = 0.0;
+    }
 
-//     swerve.driveRobotRelative(
-//         ChassisSpeeds.fromFieldRelativeSpeeds(
-//             0, 0, thetaVelocity, currPose.getRotation()));
-//   }
+    // swerve.applyRequest(
+    //     () -> drive.withRotationalRate(thetaVelocity)
+    // );
 
-//   public boolean atGoal() {
-//     return thetaController.atGoal();
-//   }
+  }
 
-//   @Override
-//   public boolean isFinished(){
-//     return atGoal();
-//     // return true;
-//   }
+  public boolean atGoal() {
+    return thetaController.atGoal();
+  }
 
-//   @Override
-//   public void end(boolean interrupted){
-//     TeleopSwerve.goodStraightGyro = swerve.getPose().getRotation();
-//     Constants.Vision.autoAlignActice = false;
-//   }
-// }
+  @Override
+  public boolean isFinished(){
+    return atGoal();
+    // return true;
+  }
+
+  @Override
+  public void end(boolean interrupted){
+    Constants.Vision.autoAlignActice = false;
+  }
+}
